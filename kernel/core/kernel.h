34860a79e277115aed816026c99ca66bcda818c4
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2020-03-15 18:43:37 +0000

    Update files

---
+++ kernel/core/kernel.h
@@ -0,0 +1,16 @@
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include <runtime/alloc.h>
+#include <runtime/libc.h>
+#include <runtime/string.h>
+
+
+
+
+
+#include <io.h>
+#include <architecture.h>
+
+#endif
---
+++ kernel/core/os.h
@@ -0,0 +1,17 @@
+#ifndef OS_H
+#define OS_H
+
+#include <config.h>
+#include <kernel.h>
+
+
+
+/*
+ *	Asm Macro
+ */
+#define asm 		__asm__
+#define asmv 		__asm__ __volatile__
+
+
+
+#endif
