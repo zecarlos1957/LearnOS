324f4e2aecf30f94f8369d23a465be4b4b035696
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2020-03-19 00:32:27 +0000

    Indent .d files

--- kernel/core/idt.d
+++ kernel/core/idt.d
@@ -1,4 +1,13 @@
-idt.o: kernel/core/idt.cc kernel/include/system.h kernel/include/io.h \
- kernel/include/system.h kernel/include/gdt.h kernel/include/idt.h \
- kernel/include/isr.h kernel/include/display.h kernel/include/memory.h \
- kernel/include/timer.h kernel/include/common.h kernel/include/keyboard.h
+idt.o: \
+kernel/core/idt.cpp \
+kernel/include/system.h \
+kernel/include/io.h \
+kernel/include/system.h \
+kernel/include/gdt.h \
+kernel/include/idt.h \
+kernel/include/isr.h \
+kernel/include/display.h \
+kernel/include/memory.h \
+kernel/include/timer.h \
+kernel/include/common.h \
+kernel/include/keyboard.h
