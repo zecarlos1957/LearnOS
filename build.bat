588f10b833a796fb7895a07f7282a5d80e4e5880
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2020-03-21 17:27:25 +0000

    Clean duplicated flags in build.bat

--- build.bat
+++ build.bat
@@ -12,50 +12,50 @@ cd ../libcpp/src
     g++ -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../libc/include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/cstd.o cstd.cpp
  
 cd ../../libc/src 
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/assert.o assert.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/ctype.o ctype.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/errno.o errno.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/stdio.o stdio.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/stdlib.o stdlib.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/string.o string.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/assert.o assert.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/ctype.o ctype.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/errno.o errno.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/stdio.o stdio.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/stdlib.o stdlib.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../include -I../../kernel/include -I../../kb_layouts -c -o ../../bin/string.o string.c
 
 cd ../../kernel/libk/disk
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I ../../../kb_layouts -c -o ../../../bin/fdc.o fdc.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I ../../../kb_layouts -c -o ../../../bin/fdc.o fdc.c
 
 cd ../hal/cpu 
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/cpustat.o cpustat.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/gdt.o gdt.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/idt.o idt.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/isr.o isr.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/cpustat.o cpustat.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/gdt.o gdt.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/idt.o idt.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../../libc/include -I../../../include -I ../../../../kb_layouts -c -o ../../../../bin/isr.o isr.c
     nasm -f win32 gdt.asm -o ../../../../bin/_gdt.o
   
 cd ../
-     gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/hal.o hal.c
-     gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/irq.o irq.c
-     gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/pic.o pic.c
-     gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/pit.o pit.c
+     gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/hal.o hal.c
+     gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/irq.o irq.c
+     gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/pic.o pic.c
+     gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/pit.o pit.c
  
 cd ../io
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/keyboard.o keyboard.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/ps2.o ps2.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/tty.o tty.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/vga.o vga.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/keyboard.o keyboard.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/ps2.o ps2.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/tty.o tty.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/vga.o vga.c
  
 cd ../memory
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/memory.o memory.c
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/paging.o paging.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/memory.o memory.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../../libc/include -I../../include -I../../../kb_layouts -c -o ../../../bin/paging.o paging.c
  
 cd ../
-    gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../libc/include -I../include -I../../kb_layouts -c -o ../../bin/panic.o panic.c
+    gcc -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../../libc/include -I../include -I../../kb_layouts -c -o ../../bin/panic.o panic.c
  
 cd ../
-  gcc -c -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../libc/include -Iinclude -I../kb_layouts -c -o ../bin/main.o main.cpp
-   nasm -f win32 kernel.s -o ../bin/kernel.o
+    g++ -ffreestanding -Wall -Wextra -m32 -nostdlib -lgcc  -fno-builtin -nostdinc -D ARCH_I386 -D KRNL_BUILD -I../libc/include -Iinclude -I../kb_layouts -c -o ../bin/main.o main.cpp
+    nasm -f win32 kernel.s -o ../bin/kernel.o
     
 cd ../bin 
-   ar rsc libc.a assert.o ctype.o errno.o stdio.o stdlib.o string.o cstd.o
-   ar rsc libcpu.a  gdt.o idt.o _gdt.o hal.o irq.o pit.o pic.o isr.o cpustat.o 
-   ar rsc libio.a  keyboard.o ps2.o tty.o vga.o memory.o panic.o paging.o fdc.o   
+    ar rsc libc.a assert.o ctype.o errno.o stdio.o stdlib.o string.o cstd.o
+    ar rsc libcpu.a  gdt.o idt.o _gdt.o hal.o irq.o pit.o pic.o isr.o cpustat.o 
+    ar rsc libio.a  keyboard.o ps2.o tty.o vga.o memory.o panic.o paging.o fdc.o   
 
 echo linking kernel...
     ld  -T ../kernel_linker.ld -o  krnl32.exe kernel.o main.o libc.a libcpu.a libio.a
@@ -67,16 +67,16 @@ echo Build floppy image...
  
 
      path  d:/tools
-    partcopy bin/boot.bin 0 200 kdev.img 0 
+    partcopy bin/boot.bin 0 200 learnos.img 0 
 
 echo Mounting disk image...
-    imdisk -a -f kdev.img -s 1440K -m B:
+    imdisk -a -f learnos.img -s 1440K -m B:
     copy  bin\KRNLDR.SYS B:\
     copy  bin\KRNL.SYS B:\
 
 echo Dismount disk...
     imdisk -D -m B:
-   d:/programas/oracle/virtualbox/vboxmanage startvm Toaruos
+   d:/programas/oracle/virtualbox/vboxmanage startvm LearnOS
 :done
 echo Done.
 echo on
