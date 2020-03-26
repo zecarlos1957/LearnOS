00d2d0152bac52d93ca460feddafe3fb89eecce0
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2020-03-26 00:42:01 +0000

    [KERNEL] Implement cpu_vendor_name func

--- kernel/include/kernel/hal/cpu/cpustat.h
+++ kernel/include/kernel/hal/cpu/cpustat.h
@@ -3,17 +3,25 @@
 #include <stdint.h>
 #include <stddef.h>
 
-struct register_set
+typedef struct 
 {
-    uint32_t di;
-    uint32_t si;
-    uint32_t bp;
-    uint32_t sp;
-    uint32_t b;
-    uint32_t d;
-    uint32_t c;
-    uint32_t a;
-};
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+}register_set;
+
+typedef struct
+{
+	register_set dregs;
+	uint32_t ds, es, fs, gs;
+	uint32_t which_int, err_code;
+	uint32_t eip, cs, eflags, user_esp, user_ss;
+} __attribute__((packed)) regs_t;
 
 #ifdef __cplusplus
 extern "C" {
@@ -22,15 +30,15 @@ extern "C" {
 
 
 
-static inline __attribute__((__always_inline__)) struct register_set get_registers(void)
+static inline __attribute__((__always_inline__)) register_set get_registers(void)
 {
-    struct register_set regset;
+    register_set regset;
     __asm__ __volatile__ (
         "pusha;"
          "mov  %%esp, %0;"
         "add  %1, %%esp;":
          "=g"(regset):
-        "Z"(sizeof(struct register_set))
+        "Z"(sizeof(register_set))
     ); 
     return regset;
 }
@@ -48,7 +56,7 @@ static inline __attribute__((__always_inline__)) uint32_t get_eflags(void)
 
 struct cpustat
 {
-    struct register_set regset;
+     register_set regset;
     uint32_t eflags;
 };
 
@@ -61,6 +69,7 @@ static inline __attribute__((__always_inline__)) struct cpustat collect_cpustat(
 }
 
 int dump_cpustat(char *buffer, size_t buffer_size, const struct cpustat cs);
+uint32_t cpu_vendor_name(char *name);
 
 #ifdef __cplusplus
 }
--- kernel/libk/hal/cpu/cpustat.c
+++ kernel/libk/hal/cpu/cpustat.c
@@ -1,7 +1,52 @@
 #include <stddef.h>
+#include <string.h>
 #include <kernel/panic.h>
 
 #include <kernel/hal/cpu/cpustat.h>
+
+#ifdef __cplusplus__
+extern "C" {
+#endif
+
+register_set cpu_cpuid(int code)
+{
+	register_set r;
+	asm volatile("cpuid":"=a"(r.eax),"=b"(r.ebx),
+                 "=c"(r.ecx),"=d"(r.edx):"0"(code));
+	return r;
+}
+
+
+uint32_t cpu_vendor_name(char *name)
+{
+		register_set r = cpu_cpuid(0x00);
+		
+		char line1[5];
+		line1[0] = ((char *) &r.ebx)[0];
+		line1[1] = ((char *) &r.ebx)[1];
+		line1[2] = ((char *) &r.ebx)[2];
+		line1[3] = ((char *) &r.ebx)[3];
+		line1[4] = '\0';
+
+		char line2[5];
+		line2[0] = ((char *) &r.ecx)[0];
+		line2[1] = ((char *) &r.ecx)[1];
+		line2[2] = ((char *) &r.ecx)[2];
+		line2[3] = ((char *) &r.ecx)[3];
+		line2[4] = '\0';
+		
+		char line3[5];
+		line3[0] = ((char *) &r.edx)[0];
+		line3[1] = ((char *) &r.edx)[1];
+		line3[2] = ((char *) &r.edx)[2];
+		line3[3] = ((char *) &r.edx)[3];
+		line3[4] = '\0';
+							
+		strcpy(name, line1);
+		strcat(name, line3);
+		strcat(name, line2);
+		return 15;
+}
 
 int dump_cpustat(char *buffer, size_t buffer_size, const struct cpustat cs)
 {
@@ -10,3 +55,7 @@ int dump_cpustat(char *buffer, size_t buffer_size, const struct cpustat cs)
     kernel_panic("Unimplemented dump_cpustat %d %d\n",buffer_size,cs.eflags);
     return -1;
 }
+
+#ifdef __cplusplus__
+}
+#endif
